020c449e28dfd54a0d72e0c5792401d57248153d
hiro0908 <hiro54924@gmail.com>
2026-04-20 07:04:58 +0900

    データベース型定義

---
+++ lib/dri/standards.ts
@@ -0,0 +1,26 @@
+//DRI食事摂取基準
+export const DRI = {
+    calories: 2000,
+    protain: 65,
+    iron: 10.5,
+    calcium: 800,
+    vitaminC: 100,
+};
+
+export const NUTRIENT_LABELS = {
+    calories: "カロリー",
+    protain: "タンパク質",
+    iron: "鉄分",
+    calusiom:"カルシウム",
+    vitaminC: "ビタミンC"
+};
+
+export const NUTRIENT_UNIT = { 
+    calories: "kcal",
+    protain: "g",
+    iron: "mg",
+    calusium: "mg",
+    vitaminC: "mg",
+}; 
+
+
---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+export type NutrientKey = 'calories' | 'proteins' | 'iron' | 'calcium' | 'vitaminC';
+
+export type FoodItem = {
+    id: number,
+    name: string,
+    category: string,
+    calories: number,
+    protain: number,
+    iron: number,
+    calcium: number,
+    vitaminC: number,
+    selectSize: string | null //1回あたりの標準的な摂取目安量
+};
+
+export type MealRecord = {
+    food: FoodItem,
+    qty: number //個数
+}
